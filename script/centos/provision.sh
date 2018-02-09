72692b5f8bf51d3852aad4afcee330fa93811d2e
momoirozo2000 <9127468+momoirozo2000@users.noreply.github.com>
2018-02-09 20:49:49 +0900

    Create provision.sh

---
+++ script/centos/provision.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+sudo yum -y install epel-release
+
+sudo yum -y update
+sudo yum -y install ansible
+
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
+sudo yum -y check-update
+sudo yum -y install azure-cli
